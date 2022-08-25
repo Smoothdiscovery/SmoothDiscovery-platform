07dcac0e8abc118e3d60ff8acf86382e94464cd4
Smoothdiscovery <101792528+Smoothdiscovery@users.noreply.github.com>
2022-08-25 17:17:43 +0800

    Create Order.sol

---
+++ src/contracts/libraries/Order.sol
@@ -0,0 +1,131 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+pragma solidity ^0.7.6;
+
+import "../interfaces/IERC20.sol";
+
+
+library Order {
+    
+    struct Data {
+        IERC20 sellToken;
+        IERC20 buyToken;
+        address receiver;
+        uint256 sellAmount;
+        uint256 buyAmount;
+        uint32 validTo;
+        bytes32 appData;
+        uint256 feeAmount;
+        bytes32 kind;
+        bool partiallyFillable;
+        bytes32 sellTokenBalance;
+        bytes32 buyTokenBalance;
+    }
+
+   
+    bytes32 internal constant TYPE_HASH =
+        hex"d5a25ba2e97094ad7d83dc28a6572da797d6b3e7fc6663bd93efb789fc17e489";
+
+   
+    bytes32 internal constant KIND_SELL =
+        hex"f3b277728b3fee749481eb3e0b3b48980dbbab78658fc419025cb16eee346775";
+
+    
+    bytes32 internal constant KIND_BUY =
+        hex"6ed88e868af0a1983e3886d5f3e95a2fafbd6c3450bc229e27342283dc429ccc";
+
+    
+    bytes32 internal constant BALANCE_ERC20 =
+        hex"5a28e9363bb942b639270062aa6bb295f434bcdfc42c97267bf003f272060dc9";
+
+    
+    bytes32 internal constant BALANCE_EXTERNAL =
+        hex"abee3b73373acd583a130924aad6dc38cfdc44ba0555ba94ce2ff63980ea0632";
+
+    
+    bytes32 internal constant BALANCE_INTERNAL =
+        hex"4ac99ace14ee0a5ef932dc609df0943ab7ac16b7583634612f8dc35a4289a6ce";
+
+   
+    address internal constant RECEIVER_SAME_AS_OWNER = address(0);
+
+    /// @dev The byte length of an order unique identifier.
+    uint256 internal constant UID_LENGTH = 56;
+
+    
+    function actualReceiver(Data memory order, address owner)
+        internal
+        pure
+        returns (address receiver)
+    {
+        if (order.receiver == RECEIVER_SAME_AS_OWNER) {
+            receiver = owner;
+        } else {
+            receiver = order.receiver;
+        }
+    }
+
+    
+    function hash(Data memory order, bytes32 domainSeparator)
+        internal
+        pure
+        returns (bytes32 orderDigest)
+    {
+        bytes32 structHash;
+
+        
+        assembly {
+            let dataStart := sub(order, 32)
+            let temp := mload(dataStart)
+            mstore(dataStart, TYPE_HASH)
+            structHash := keccak256(dataStart, 416)
+            mstore(dataStart, temp)
+        }
+
+        
+        assembly {
+            let freeMemoryPointer := mload(0x40)
+            mstore(freeMemoryPointer, "\x19\x01")
+            mstore(add(freeMemoryPointer, 2), domainSeparator)
+            mstore(add(freeMemoryPointer, 34), structHash)
+            orderDigest := keccak256(freeMemoryPointer, 66)
+        }
+    }
+
+    
+    function packOrderUidParams(
+        bytes memory orderUid,
+        bytes32 orderDigest,
+        address owner,
+        uint32 validTo
+    ) internal pure {
+        require(orderUid.length == UID_LENGTH, "GPv2: uid buffer overflow");
+
+        
+        assembly {
+            mstore(add(orderUid, 56), validTo)
+            mstore(add(orderUid, 52), owner)
+            mstore(add(orderUid, 32), orderDigest)
+        }
+    }
+
+    
+    function extractOrderUidParams(bytes calldata orderUid)
+        internal
+        pure
+        returns (
+            bytes32 orderDigest,
+            address owner,
+            uint32 validTo
+        )
+    {
+        require(orderUid.length == UID_LENGTH, "GPv2: invalid uid");
+
+        // Use assembly to efficiently decode packed calldata.
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            orderDigest := calldataload(orderUid.offset)
+            owner := shr(96, calldataload(add(orderUid.offset, 32)))
+            validTo := shr(224, calldataload(add(orderUid.offset, 52)))
+        }
+    }
+}
