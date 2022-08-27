778825f0593059dcd7684d3cf8d5b5b62cf52034
Smoothdiscovery <101792528+Smoothdiscovery@users.noreply.github.com>
2022-08-27 16:53:36 +0800

    Create asyAuction.sol

---
+++ src/contracts/asyAuction.sol
@@ -0,0 +1,266 @@
+pragma solidity ^0.7.5;
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "./libraries/IterableOrderedOrderSet.sol";
+import "@openzeppelin/contracts/math/Math.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "./libraries/IdToAddressBiMap.sol";
+import "./libraries/SafeCast.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./interfaces/AllowListVerifier.sol";
+
+contract EasyAuction is Ownable {
+    using SafeERC20 for IERC20;
+    using SafeMath for uint64;
+    using SafeMath for uint96;
+    using SafeMath for uint256;
+    using SafeCast for uint256;
+    using IterableOrderedOrderSet for IterableOrderedOrderSet.Data;
+    using IterableOrderedOrderSet for bytes32;
+    using IdToAddressBiMap for IdToAddressBiMap.Data;
+
+    modifier atStageOrderPlacement(uint256 auctionId) {
+        require(
+            block.timestamp < auctionData[auctionId].auctionEndDate,
+            "no longer in order placement phase"
+        );
+        _;
+    }
+
+    modifier atStageOrderPlacementAndCancelation(uint256 auctionId) {
+        require(
+            block.timestamp < auctionData[auctionId].orderCancellationEndDate,
+            "no longer in order placement and cancelation phase"
+        );
+        _;
+    }
+
+    modifier atStageSolutionSubmission(uint256 auctionId) {
+        {
+            uint256 auctionEndDate = auctionData[auctionId].auctionEndDate;
+            require(
+                auctionEndDate != 0 &&
+                    block.timestamp >= auctionEndDate &&
+                    auctionData[auctionId].clearingPriceOrder == bytes32(0),
+                "Auction not in solution submission phase"
+            );
+        }
+        _;
+    }
+
+    modifier atStageFinished(uint256 auctionId) {
+        require(
+            auctionData[auctionId].clearingPriceOrder != bytes32(0),
+            "Auction not yet finished"
+        );
+        _;
+    }
+
+    event NewSellOrder(
+        uint256 indexed auctionId,
+        uint64 indexed userId,
+        uint96 buyAmount,
+        uint96 sellAmount
+    );
+    event CancellationSellOrder(
+        uint256 indexed auctionId,
+        uint64 indexed userId,
+        uint96 buyAmount,
+        uint96 sellAmount
+    );
+    event ClaimedFromOrder(
+        uint256 indexed auctionId,
+        uint64 indexed userId,
+        uint96 buyAmount,
+        uint96 sellAmount
+    );
+    event NewUser(uint64 indexed userId, address indexed userAddress);
+    event NewAuction(
+        uint256 indexed auctionId,
+        IERC20 indexed _auctioningToken,
+        IERC20 indexed _biddingToken,
+        uint256 orderCancellationEndDate,
+        uint256 auctionEndDate,
+        uint64 userId,
+        uint96 _auctionedSellAmount,
+        uint96 _minBuyAmount,
+        uint256 minimumBiddingAmountPerOrder,
+        uint256 minFundingThreshold,
+        address allowListContract,
+        bytes allowListData
+    );
+    event AuctionCleared(
+        uint256 indexed auctionId,
+        uint96 soldAuctioningTokens,
+        uint96 soldBiddingTokens,
+        bytes32 clearingPriceOrder
+    );
+    event UserRegistration(address indexed user, uint64 userId);
+
+    struct AuctionData {
+        IERC20 auctioningToken;
+        IERC20 biddingToken;
+        uint256 orderCancellationEndDate;
+        uint256 auctionEndDate;
+        bytes32 initialAuctionOrder;
+        uint256 minimumBiddingAmountPerOrder;
+        uint256 interimSumBidAmount;
+        bytes32 interimOrder;
+        bytes32 clearingPriceOrder;
+        uint96 volumeClearingPriceOrder;
+        bool minFundingThresholdNotReached;
+        bool isAtomicClosureAllowed;
+        uint256 feeNumerator;
+        uint256 minFundingThreshold;
+    }
+    mapping(uint256 => IterableOrderedOrderSet.Data) internal sellOrders;
+    mapping(uint256 => AuctionData) public auctionData;
+    mapping(uint256 => address) public auctionAccessManager;
+    mapping(uint256 => bytes) public auctionAccessData;
+
+    IdToAddressBiMap.Data private registeredUsers;
+    uint64 public numUsers;
+    uint256 public auctionCounter;
+
+    constructor() public Ownable() {}
+
+    uint256 public feeNumerator = 0;
+    uint256 public constant FEE_DENOMINATOR = 1000;
+    uint64 public feeReceiverUserId = 1;
+
+    function setFeeParameters(
+        uint256 newFeeNumerator,
+        address newfeeReceiverAddress
+    ) public onlyOwner() {
+        require(
+            newFeeNumerator <= 15,
+            "Fee is not allowed to be set higher than 1.5%"
+        );
+        // caution: for currently running auctions, the feeReceiverUserId is changing as well.
+        feeReceiverUserId = getUserId(newfeeReceiverAddress);
+        feeNumerator = newFeeNumerator;
+    }
+
+    // @dev: function to intiate a new auction
+    // Warning: In case the auction is expected to raise more than
+    // 2^96 units of the biddingToken, don't start the auction, as
+    // it will not be settlable. This corresponds to about 79
+    // billion DAI.
+    //
+    // Prices between biddingToken and auctioningToken are expressed by a
+    // fraction whose components are stored as uint96.
+    function initiateAuction(
+        IERC20 _auctioningToken,
+        IERC20 _biddingToken,
+        uint256 orderCancellationEndDate,
+        uint256 auctionEndDate,
+        uint96 _auctionedSellAmount,
+        uint96 _minBuyAmount,
+        uint256 minimumBiddingAmountPerOrder,
+        uint256 minFundingThreshold,
+        bool isAtomicClosureAllowed,
+        address accessManagerContract,
+        bytes memory accessManagerContractData
+    ) public returns (uint256) {
+        // withdraws sellAmount + fees
+        _auctioningToken.safeTransferFrom(
+            msg.sender,
+            address(this),
+            _auctionedSellAmount.mul(FEE_DENOMINATOR.add(feeNumerator)).div(
+                FEE_DENOMINATOR
+            ) //[0]
+        );
+        require(_auctionedSellAmount > 0, "cannot auction zero tokens");
+        require(_minBuyAmount > 0, "tokens cannot be auctioned for free");
+        require(
+            minimumBiddingAmountPerOrder > 0,
+            "minimumBiddingAmountPerOrder is not allowed to be zero"
+        );
+        require(
+            orderCancellationEndDate <= auctionEndDate,
+            "time periods are not configured correctly"
+        );
+        require(
+            auctionEndDate > block.timestamp,
+            "auction end date must be in the future"
+        );
+        auctionCounter = auctionCounter.add(1);
+        sellOrders[auctionCounter].initializeEmptyList();
+        uint64 userId = getUserId(msg.sender);
+        auctionData[auctionCounter] = AuctionData(
+            _auctioningToken,
+            _biddingToken,
+            orderCancellationEndDate,
+            auctionEndDate,
+            IterableOrderedOrderSet.encodeOrder(
+                userId,
+                _minBuyAmount,
+                _auctionedSellAmount
+            ),
+            minimumBiddingAmountPerOrder,
+            0,
+            IterableOrderedOrderSet.QUEUE_START,
+            bytes32(0),
+            0,
+            false,
+            isAtomicClosureAllowed,
+            feeNumerator,
+            minFundingThreshold
+        );
+        auctionAccessManager[auctionCounter] = accessManagerContract;
+        auctionAccessData[auctionCounter] = accessManagerContractData;
+        emit NewAuction(
+            auctionCounter,
+            _auctioningToken,
+            _biddingToken,
+            orderCancellationEndDate,
+            auctionEndDate,
+            userId,
+            _auctionedSellAmount,
+            _minBuyAmount,
+            minimumBiddingAmountPerOrder,
+            minFundingThreshold,
+            accessManagerContract,
+            accessManagerContractData
+        );
+        return auctionCounter;
+    }
+
+    function registerUser(address user) public returns (uint64 userId) {
+        numUsers = numUsers.add(1).toUint64();
+        require(
+            registeredUsers.insert(numUsers, user),
+            "User already registered"
+        );
+        userId = numUsers;
+        emit UserRegistration(user, userId);
+    }
+
+    function getUserId(address user) public returns (uint64 userId) {
+        if (registeredUsers.hasAddress(user)) {
+            userId = registeredUsers.getId(user);
+        } else {
+            userId = registerUser(user);
+            emit NewUser(userId, user);
+        }
+    }
+
+    function getSecondsRemainingInBatch(uint256 auctionId)
+        public
+        view
+        returns (uint256)
+    {
+        if (auctionData[auctionId].auctionEndDate < block.timestamp) {
+            return 0;
+        }
+        return auctionData[auctionId].auctionEndDate.sub(block.timestamp);
+    }
+
+    function containsOrder(uint256 auctionId, bytes32 order)
+        public
+        view
+        returns (bool)
+    {
+        return sellOrders[auctionId].contains(order);
+    }
+}
